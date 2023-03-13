7f9ed16d863e5c6cfcc5a8072b972b1620da272f
Xabier Rodriguez-Calvar <calvaris@igalia.com>
2023-03-13 06:52:46 -0700

    [GStreamer][MediaStream][Realtek] configure 'immediate-output' for audio sink https://bugs.webkit.org/show_bug.cgi?id=253439
    Reviewed by Philippe Normand.
    
    Patch based on one of Eugene Mutavchi <Ievgen_Mutavchi@comcast.com> and another from Gurdal Oruklu <gurdaloruklu@gmail.com>.
    
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::configureElement):
    (WebCore::MediaPlayerPrivateGStreamer::configureElementPlatformQuirks):
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    
    Canonical link: https://commits.webkit.org/261569@main

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -521,7 +521,7 @@ private:
     void configureDepayloader(GstElement*);
     void configureVideoDecoder(GstElement*);
     void configureElement(GstElement*);
-#if PLATFORM(BROADCOM) || USE(WESTEROS_SINK) || PLATFORM(AMLOGIC)
+#if PLATFORM(BROADCOM) || USE(WESTEROS_SINK) || PLATFORM(AMLOGIC) || PLATFORM(REALTEK)
     void configureElementPlatformQuirks(GstElement*);
 #endif
 
