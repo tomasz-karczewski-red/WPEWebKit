tomasz-karczewski-red/WPEWebKit

commit 086dc8f2b7cc738beecde2e20bb27b3b4ab078e3

[GStreamer] Clean and document custom GStreamer element ranks https://bugs.webkit.org/show_bug.cgi?id=251986
Reviewed by Xabier Rodriguez-Calvar.

Guidelines for rank assignment were added and elements ranks updated accordingly.

* Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.cpp:
(WebCore::registerWebKitGStreamerElements):

Canonical link: https://commits.webkit.org/260104@main

Signed-off-by: Xabier Rodriguez Calvar <calvaris@igalia.com>

Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.cpp
@@ -327,6 +327,13 @@ void registerWebKitGStreamerElements()
     bool registryWasUpdated = false;
     std::call_once(onceFlag, [&registryWasUpdated] {
 
+        // Rank guidelines are as following:
+        // - Use GST_RANK_PRIMARY for elements meant to be auto-plugged and for which we know
+        //   there's no other alternative outside of WebKit.
+        // - Use GST_RANK_PRIMARY+100 for elements meant to be auto-plugged and that we know there
+        //   is an alternative outside of WebKit.
+        // - Use GST_RANK_NONE for elements explicitely created by WebKit (no auto-plugging).
+
 #if ENABLE(ENCRYPTED_MEDIA) && ENABLE(THUNDER)
         if (!CDMFactoryThunder::singleton().supportedKeySystems().isEmpty())
             gst_element_register(nullptr, "webkitthunder", GST_RANK_PRIMARY + 100, WEBKIT_TYPE_MEDIA_THUNDER_DECRYPT);
@@ -338,7 +345,11 @@ void registerWebKitGStreamerElements()
         registerInternalVideoEncoder();
 
 #if ENABLE(MEDIA_SOURCE)
-        gst_element_register(nullptr, "webkitmediasrc", GST_RANK_PRIMARY + 100, WEBKIT_TYPE_MEDIA_SRC);
+        gst_element_register(nullptr, "webkitmediasrc", GST_RANK_PRIMARY, WEBKIT_TYPE_MEDIA_SRC);
+#endif
+
+#if ENABLE(SPEECH_SYNTHESIS)
+        gst_element_register(nullptr, "webkitflitesrc", GST_RANK_NONE, WEBKIT_TYPE_FLITE_SRC);
 #endif
 
 #if ENABLE(VIDEO)

commit 9e4ac771e6b827f132565beb463322da63ba38e9

WebSpeech: fix a compilation
With ENABLE_SPEECH_SYNTHESIS compilation of the WPE is not possible due to the error:

Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.cpp:352:72: error: ‘WEBKIT_TYPE_FLITE_SRC’ was not declared in this scope; did you mean ‘WEBKIT_TYPE_MEDIA_SRC’?
  352 |         gst_element_register(nullptr, "webkitflitesrc", GST_RANK_NONE, WEBKIT_TYPE_FLITE_SRC);

The problem was introduced by:
https://github.com/WebPlatformForEmbedded/WPEWebKit/commit/086dc8f2b7cc738beecde2e20bb27b3b4ab078e3

To fix it the registration of the 'webkitflitesrc' has to be removed - flite is not supported in this version of WPE.

Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.cpp
@@ -348,10 +348,6 @@ void registerWebKitGStreamerElements()
         gst_element_register(nullptr, "webkitmediasrc", GST_RANK_PRIMARY, WEBKIT_TYPE_MEDIA_SRC);
 #endif
 
-#if ENABLE(SPEECH_SYNTHESIS)
-        gst_element_register(nullptr, "webkitflitesrc", GST_RANK_NONE, WEBKIT_TYPE_FLITE_SRC);
-#endif
-
 #if ENABLE(VIDEO)
         gst_element_register(0, "webkitwebsrc", GST_RANK_PRIMARY + 100, WEBKIT_TYPE_WEB_SRC);
         gst_element_register(0, "webkitdmabufvideosink", GST_RANK_NONE, WEBKIT_TYPE_DMABUF_VIDEO_SINK);