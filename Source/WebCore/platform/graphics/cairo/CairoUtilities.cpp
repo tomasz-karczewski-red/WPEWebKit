ee006a258bb754ca841b429c886c701ad1a5fe4f
Miguel Gomez <magomez@igalia.com>
2023-12-13 15:36:10 -0800

    [GTK][WPE] Random incorrect image displayed as the background of a div https://bugs.webkit.org/show_bug.cgi?id=265990
    Reviewed by Žan Doberšek.
    
    Add an unique ID to cairo surfaces created in ImageBackingStoreCairo. This unique ID allows us
    to differentiate when the cairo surface that's backing an image has changed, so we are sure that
    we're not using an incorrect value cached inside some ImageBackingStore.
    
    * Source/WebCore/platform/graphics/cairo/CairoUtilities.cpp:
    (WebCore::attachSurfaceUniqueID):
    (WebCore::getSurfaceUniqueID):
    * Source/WebCore/platform/graphics/cairo/CairoUtilities.h:
    * Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly):
    * Source/WebCore/platform/image-decoders/cairo/ImageBackingStoreCairo.cpp:
    (WebCore::ImageBackingStore::image const):
    
    Canonical link: https://commits.webkit.org/272009@main

--- Source/WebCore/platform/graphics/cairo/CairoUtilities.cpp
+++ Source/WebCore/platform/graphics/cairo/CairoUtilities.cpp
@@ -39,6 +39,7 @@
 #include "RefPtrCairo.h"
 #include "Region.h"
 #include <wtf/Assertions.h>
+#include <wtf/Atomics.h>
 #include <wtf/NeverDestroyed.h>
 #include <wtf/UniqueArray.h>
 #include <wtf/Vector.h>
@@ -52,6 +53,9 @@
 #endif
 
 namespace WebCore {
+
+static cairo_user_data_key_t s_surfaceUniqueIDKey;
+static Atomic<uintptr_t> s_surfaceUniqueID = 1;
 
 #if USE(FREETYPE)
 RecursiveLock& cairoFontLock()
@@ -379,6 +383,16 @@ RefPtr<cairo_region_t> toCairoRegion(const Region& region)
 cairo_matrix_t toCairoMatrix(const AffineTransform& transform)
 {
     return cairo_matrix_t { transform.a(), transform.b(), transform.c(), transform.d(), transform.e(), transform.f() };
+}
+
+void attachSurfaceUniqueID(cairo_surface_t* surface)
+{
+    cairo_surface_set_user_data(surface, &s_surfaceUniqueIDKey, reinterpret_cast<void*>(s_surfaceUniqueID.exchangeAdd(1)), nullptr);
+}
+
+uintptr_t getSurfaceUniqueID(cairo_surface_t* surface)
+{
+    return reinterpret_cast<uintptr_t>(cairo_surface_get_user_data(surface, &s_surfaceUniqueIDKey));
 }
 
 } // namespace WebCore
--- Source/WebCore/platform/graphics/cairo/CairoUtilities.h
+++ Source/WebCore/platform/graphics/cairo/CairoUtilities.h
@@ -99,6 +99,9 @@ RefPtr<cairo_region_t> toCairoRegion(const Region&);
 
 cairo_matrix_t toCairoMatrix(const AffineTransform&);
 
+void attachSurfaceUniqueID(cairo_surface_t*);
+uintptr_t getSurfaceUniqueID(cairo_surface_t*);
+
 } // namespace WebCore
 
 #endif // USE(CAIRO)
