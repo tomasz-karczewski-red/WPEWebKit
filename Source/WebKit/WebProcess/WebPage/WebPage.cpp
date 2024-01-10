231279cb0d17df77b5a623d5a0a716d5c4217f45
Filipe Norte <filipe.norte@sky.uk>
2024-01-10 15:22:20 +0000

    Fix Security Origin handling in network process
    Currently, when a custom uri scheme handler is registered, its
    existence is not passed to the network process. Consequently,
    when creating a SecurityOrigin object for an URI that uses a custom
    scheme handler, the instance may be created as unique due
    shouldTreatAsUniqueOrigin() not detecting the associated scheme as
    registered (in LegacySchemeRegistry).
    
    This will cause calls to SecurityPolicy::isAccessAllowed() to not
    return the correct authorization in case a custom URI is whitelisted
    using webkit_web_extension_add_origin_access_whitelist_entry() API,
    which leads to the inclusion of the "Origin" header with the custom URI
    in network requests when it should not be included in such case.

--- Source/WebKit/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -7285,6 +7285,8 @@ void WebPage::registerURLSchemeHandler(WebURLSchemeHandlerIdentifier handlerIden
     WebCore::LegacySchemeRegistry::registerURLSchemeAsCORSEnabled(scheme);
     auto schemeResult = m_schemeToURLSchemeHandlerProxyMap.add(scheme, WebURLSchemeHandlerProxy::create(*this, handlerIdentifier));
     m_identifierToURLSchemeHandlerProxyMap.add(handlerIdentifier, schemeResult.iterator->value.get());
+
+    WebProcess::singleton().ensureNetworkProcessConnection().connection().send(Messages::NetworkConnectionToWebProcess::RegisterURLSchemeAsHandledBySchemeHandler { scheme }, 0);
 }
 
 void WebPage::urlSchemeTaskWillPerformRedirection(WebURLSchemeHandlerIdentifier handlerIdentifier, WebCore::ResourceLoaderIdentifier taskIdentifier, ResourceResponse&& response, ResourceRequest&& request, CompletionHandler<void(WebCore::ResourceRequest&&)>&& completionHandler)
