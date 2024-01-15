WebKit/WebKit

[MSE][GStreamer] Dimension SourceBuffer size limit for all possible track types before init segment received https://bugs.webkit.org/show_bug.cgi?id=267417
Reviewed by Philippe Normand.

SourceBuffer has separate max size limits for video, audio and text, but it has to parse the
init segment first to know if there are any video, audio or text tracks. At the very first
appendBuffer() there are not tracks parsed yet, so platformMaximumBufferSize() returns 0 (as
no tracks are there), and the limit is taken from MediaElementSession as audio limit 15MB as
no video track is present.

See: https://github.com/WebPlatformForEmbedded/WPEWebKit/pull/1261

Inspired by the work of Andrzej Surdej <Andrzej_Surdej@comcast.com>.

This patch adjusts the platform limits for WPE so that they account for the possibility of
any (or even all) of the possible track types appearing when processing the init segment.

* Source/WebCore/platform/graphics/gstreamer/mse/SourceBufferPrivateGStreamer.cpp:
(WebCore::SourceBufferPrivateGStreamer::platformMaximumBufferSize const): Accumulate all the audio, video, text maximum sizes when no tracks have been detected yet.

Canonical link: https://commits.webkit.org/273039@main

Source/WebCore/platform/graphics/gstreamer/mse/SourceBufferPrivateGStreamer.cpp
@@ -315,7 +315,10 @@ size_t SourceBufferPrivateGStreamer::platformMaximumBufferSize() const
     });
 
     // If any track type size isn't specified, we consider that it has no limit and the values from the
-    // element have to be used. Otherwise, the track limits are accumulative.
+    // element have to be used. Otherwise, the track limits are accumulative. If everything is specified
+    // but there's no track (eg: because we're processing an init segment that we don't know yet which
+    // kind of track(s) is going to generate) we assume that the 3 kind of tracks might appear (audio,
+    // video, text) and use all the accumulated limits at once to make room for any possible outcome.
     do {
         bool hasVideo = false;
         bool hasAudio = false;
@@ -338,19 +341,19 @@ size_t SourceBufferPrivateGStreamer::platformMaximumBufferSize() const
             }
         }
 
-        if (hasVideo) {
+        if (hasVideo || m_tracks.empty()) {
             if (maxBufferSizeVideo)
                 bufferSize += maxBufferSizeVideo;
             else
                 break;
         }
-        if (hasAudio) {
+        if (hasAudio || m_tracks.empty()) {
             if (maxBufferSizeAudio)
                 bufferSize += maxBufferSizeAudio;
             else
                 break;
         }
-        if (hasText) {
+        if (hasText || m_tracks.empty()) {
             if (maxBufferSizeText)
                 bufferSize += maxBufferSizeText;
             else

tomasz-karczewski-red/WPEWebKit

Source/WebCore/platform/graphics/gstreamer/mse/SourceBufferPrivateGStreamer.cpp
@@ -331,7 +331,10 @@ size_t SourceBufferPrivateGStreamer::platformMaximumBufferSize() const
     });
 
     // If any track type size isn't specified, we consider that it has no limit and the values from the
-    // element have to be used. Otherwise, the track limits are accumulative.
+    // element have to be used. Otherwise, the track limits are accumulative. If everything is specified
+    // but there's no track (eg: because we're processing an init segment that we don't know yet which
+    // kind of track(s) is going to generate) we assume that the 3 kind of tracks might appear (audio,
+    // video, text) and use all the accumulated limits at once to make room for any possible outcome.
     do {
         bool hasVideo = false;
         bool hasAudio = false;
@@ -354,19 +357,19 @@ size_t SourceBufferPrivateGStreamer::platformMaximumBufferSize() const
             }
         }
 
-        if (hasVideo) {
+        if (hasVideo || m_tracks.isEmpty()) {
             if (maxBufferSizeVideo)
                 bufferSize += maxBufferSizeVideo;
             else
                 break;
         }
-        if (hasAudio) {
+        if (hasAudio || m_tracks.isEmpty()) {
             if (maxBufferSizeAudio)
                 bufferSize += maxBufferSizeAudio;
             else
                 break;
         }
-        if (hasText) {
+        if (hasText || m_tracks.isEmpty()) {
             if (maxBufferSizeText)
                 bufferSize += maxBufferSizeText;
             else