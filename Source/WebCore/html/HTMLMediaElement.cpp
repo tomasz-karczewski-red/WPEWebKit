d8bc362da67f6fb958c904bf5f381ba7d668fd2e
Marcin Mielczarczyk <marcin.mielczarczyk@redembedded.com>
2024-05-08 10:52:25 +0200

    [YTB tests] Increase the time granularity
    This fixes YTB MSE Progressive Tests 29, 30, 32, 33 (granularity) by increasing
    the time granularity to better values (200ms on average, the spec allows a
    range from 15 to 250ms).

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -4200,7 +4200,7 @@ void HTMLMediaElement::scanTimerFired()
 
 // The spec says to fire periodic timeupdate events (those sent while playing) every
 // "15 to 250ms", we choose the slowest frequency
-static const Seconds maxTimeupdateEventFrequency { 250_ms };
+static const Seconds maxTimeupdateEventFrequency { 200_ms };
 
 void HTMLMediaElement::startPlaybackProgressTimer()
 {
