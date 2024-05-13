513affc8cea69dcdf2c54108d930d3da28e96749
Mikolaj Staworzynski <mikolaj.staworzynski@redembedded.com>
2024-05-13 13:37:44 +0200

    ARRISAPP-1018: remove some of the WPE0050 reports
    by adding and use _NO_ODH macros

--- Source/WTF/wtf/Assertions.h
+++ Source/WTF/wtf/Assertions.h
@@ -662,6 +662,7 @@ constexpr bool assertionFailureDueToUnreachableCode = false;
 #define PRIVATE_LOG_PREFIX ""
 #define RELEASE_LOG(channel, ...) LOG(channel, __VA_ARGS__)
 #define RELEASE_LOG_ERROR(channel, ...) LOG_ERROR(__VA_ARGS__)
+#define RELEASE_LOG_ERROR_NO_ODH(channel, ...) LOG_ERROR_NO_ODH(__VA_ARGS__)
 #define RELEASE_LOG_FAULT(channel, ...) FATAL(__VA_ARGS__)
 #define RELEASE_LOG_INFO(channel, ...) LOG_WITH_LEVEL(channel, 3, __VA_ARGS__)
 
