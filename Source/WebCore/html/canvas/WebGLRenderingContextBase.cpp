b16233bdb7d45e19b1f92b36d7cd8319d14bfec2
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2024-05-21 15:40:44 +0200

    ARRISAPOL-3419 webgl context leak fix
    (1) setting maxActiveContexts and maxActiveWorkerContexts
    to just '1'
    
    The applications normally use just 1 webgl context,
    and this allows to release webgl contexts sooner, without waiting
    for GC; each such context normally holds at least 32MB of GFX memory
    
    (2) calling 'reshape(0,0)' when destroying webgl context
    
    This releases the textures attached to context fbo immediately,
    again - without waiting for GC to kick in. These textures make up
    for the most of GFX memory used.
    
    (3) changing order of operations in ~WebGLRenderingContextBase
    
    Removing from the context group before the context itself is
    removed. Otherwise, WebGLContextGroup will not be able to release
    some of the 'group shared' resources (these that still have
    attachements when being removed via WebGLSharedObject destructor)
    it happens eg. for WebGLVertexArrayObjectBase objects that have
    buffers set on destruction time

--- Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
+++ Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
@@ -164,8 +164,8 @@ WTF_MAKE_ISO_ALLOCATED_IMPL(WebGLRenderingContextBase);
 static constexpr Seconds secondsBetweenRestoreAttempts { 1_s };
 static constexpr int maxGLErrorsAllowedToConsole = 256;
 static constexpr Seconds checkContextLossHandlingDelay { 3_s };
-static constexpr size_t maxActiveContexts = 16;
-static constexpr size_t maxActiveWorkerContexts = 4;
+static constexpr size_t maxActiveContexts = 1;
+static constexpr size_t maxActiveWorkerContexts = 1;
 
 namespace {
     
