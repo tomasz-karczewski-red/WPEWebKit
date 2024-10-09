fa0502a248c5370a2422b8f3fe4e5c14af3c6ba0
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2024-10-09 14:06:59 +0200

    Revert "ARRISAPOL-3419 webgl context leak fix"
    This reverts commit b2ff0ea2e543d22768dda0421d36fdb89bfda79a.

--- Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
+++ Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
@@ -164,8 +164,8 @@ WTF_MAKE_ISO_ALLOCATED_IMPL(WebGLRenderingContextBase);
 static constexpr Seconds secondsBetweenRestoreAttempts { 1_s };
 static constexpr int maxGLErrorsAllowedToConsole = 256;
 static constexpr Seconds checkContextLossHandlingDelay { 3_s };
-static constexpr size_t maxActiveContexts = 1;
-static constexpr size_t maxActiveWorkerContexts = 1;
+static constexpr size_t maxActiveContexts = 16;
+static constexpr size_t maxActiveWorkerContexts = 4;
 
 namespace {
     
@@ -1238,13 +1238,6 @@ void WebGLRenderingContextBase::removeActivityStateChangeObserver()
 
 WebGLRenderingContextBase::~WebGLRenderingContextBase()
 {
-    if (!m_isPendingPolicyResolution) {
-        // need to remove from the group first, before we destroy the graphics context
-        // othwerwise, in case this is the last context in the group, when the context group tries
-        // to cleanup the remaining objects, it will not call deleteImpl (see WebGLObject::deleteObject)
-        // since the context is no longer available
-        m_contextGroup->removeContext(*this);
-    }
     // Remove all references to WebGLObjects so if they are the last reference
     // they will be freed before the last context is removed from the context group.
     m_boundArrayBuffer = nullptr;
@@ -1269,6 +1262,7 @@ WebGLRenderingContextBase::~WebGLRenderingContextBase()
         detachAndRemoveAllObjects();
         loseExtensions(LostContextMode::RealLostContext);
         destroyGraphicsContextGL();
+        m_contextGroup->removeContext(*this);
     }
 
     {
@@ -1305,8 +1299,6 @@ void WebGLRenderingContextBase::destroyGraphicsContextGL()
     removeActivityStateChangeObserver();
 
     if (m_context) {
-        // first release the big textures allocated for the FBOs
-        m_context->reshape(0,0);
         m_context->setClient(nullptr);
         m_context = nullptr;
         removeActiveContext(*this);
